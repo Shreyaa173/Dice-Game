939a57d71b5b77531ce16b6c5b6c92c2540e9ed6
Shreya <shreya186btcse23@igdtuw.ac.in>
2024-10-30 23:42:40 +0530

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  padding: 10px;
+`;
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
